39cf0c1774e80e332323a35bb4ecb45afbe6e08a
Yusuf OLGUN <abd_yusuf@yahoo.com>
2022-04-08 16:20:30 +0300

    Car Task2

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,8 @@
+package day38_Inheritance.carTask;
+public class ParkingLot {
+    public static void main(String[] args) {
+        Toyota toyoto=new Toyota("Camry","Gray",2020,200000,123456);
+        Tesla tesla=new Tesla("MOdel S","white",2018,100000,500000);
+        BMW bmw=new BMW("X5","grey",1995,500000,1000);
+    }
+}
