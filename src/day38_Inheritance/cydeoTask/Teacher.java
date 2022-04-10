f37b58e487b7a0ab3bb525463a5ee3d368638d69
Yusuf OLGUN <abd_yusuf@yahoo.com>
2022-04-10 19:02:04 +0300

    Car Task2

---
+++ src/day38_Inheritance/cydeoTask/Teacher.java
@@ -0,0 +1,10 @@
+package day38_Inheritance.cydeoTask;
+public class Teacher extends Employee {
+    public Teacher(String name, int age, char gender, int employeeId, String jobTitle, double salary) {
+        super(name, age, gender, employeeId, jobTitle, salary);
+    }
+    @Override
+    public void work() {
+        System.out.println(getJobTitle()+" "+getName()+" is teaching ");
+    }
+}
